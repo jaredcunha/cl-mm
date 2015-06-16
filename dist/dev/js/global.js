2cd12282ce2c3cf8d4b09cbb341328e7c0612d88
Jared Cunha <jaredcunha@gmail.com>
2015-06-16 10:22:28 -0400

    make the arrow clickable

--- dist/dev/js/global.js
+++ dist/dev/js/global.js
@@ -91,6 +91,23 @@ $(document).ready(function(){
 	    'height': winHeight,
 	});
 });
+/* ===================================
+Smooth Scrolling Function
+======================================*/
+$(function() {
+  $('a[href*=#]:not([href=#])').on('click', function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
 $(document).ready(function(){
 	if (!Modernizr.svg) {
 	  $('img[src$=".svg"]').each(function() {
